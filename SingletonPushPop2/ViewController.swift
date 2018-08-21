b8ca9168cc9575a6434e1c337dfa7becf2c3aec1
trung <trung@admins-imac-2.local>
2018-08-21 09:42:18 +0700

    Initial Commit

---
+++ SingletonPushPop2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SingletonPushPop2
+//
+//  Created by trung on 2018/08/21.
+//  Copyright © 2018 trung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
